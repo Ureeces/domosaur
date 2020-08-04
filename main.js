b2acd15a4658ecaa6f66c7397e7c7818d536765d
Ureeces <Ulysses.Reyes@codeimmersives.com>
2020-08-04 14:29:54 -0400

    Finished Flex Goals

---
+++ main.js
@@ -0,0 +1,12 @@
+// Flex Goals
+const messSpan = document.querySelector('span.mess-with-me');
+messSpan.style.fontSize = "3em";
+
+const messP = document.querySelector('p.mess-with-me');
+messP.style.backgroundColor = "green";
+
+const hiddenDino = document.querySelector('#hide-me-area');
+hiddenDino.style.display = "none";
+
+const triceratops = document.querySelector('#triceratops');
+triceratops.width = "324";
